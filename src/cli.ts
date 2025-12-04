09f9820bcffe9a3c4a6ecb0b74ecba741d22aa52
TAKUMI HIRANO <thirano@TAKUMInoMacBook-Pro.local>
2025-12-04 23:53:58 +0900

    initialize papertest project

---
+++ src/cli.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// 0.0.1 は名前占有用のシンプル実装でOK
+const pkg = require('../package.json');
+
+console.log(`papertest v${pkg.version}`);
+console.log('This is a placeholder CLI for papertest 0.0.1.');
